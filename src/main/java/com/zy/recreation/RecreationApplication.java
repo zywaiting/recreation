921bac24e4a1effef50ec8791791b200686ef160
wq <yzhu@raipeng.com>
2018-05-14 14:18:43 +0800

    搭建项目

---
+++ src/main/java/com/zy/recreation/RecreationApplication.java
@@ -0,0 +1,12 @@
+package com.zy.recreation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecreationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecreationApplication.class, args);
+	}
+}
---
+++ src/main/java/com/zy/recreation/pojo/Joke.java
@@ -0,0 +1,5 @@
+package com.zy.recreation.pojo;
+
+public class Joke {
+    
+}
